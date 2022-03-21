51a0cd5df135f7c03d908e66c617b4a7aff8da6e
Hugo Levet <hugolevet.pro@gmail.com>
2022-03-21 15:55:17 +0100

    ADD footer

---
+++ src/components/Layout/NavElements/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './index.module.scss';
+
+
+const NavElements = () => {
+    return (
+        <nav className={styles.nav}>
+            <a href="">ROADMAP</a>
+            <a href="">TEAM</a>
+            <a href="">COMUNITY</a>
+            <a href="">LAUNCHPAD</a>
+            <a href="">ASSOCIATION</a>
+            <a href="">FAQ</a>
+        </nav>
+    );
+};
+
+export default NavElements;
