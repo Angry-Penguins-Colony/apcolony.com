91763bb5e77e0e0359710d650c4c2f35114249f0
Hugo Levet <hugolevet.pro@gmail.com>
2022-04-12 12:08:39 +0200

    FIX nav link

--- src/components/Layout/NavElements/index.tsx
+++ src/components/Layout/NavElements/index.tsx
@@ -11,12 +11,12 @@ const NavElements = (props: { isOpen?: boolean; }) => {
         }>
             <div className={styles.navLinksContainer}>
 
-                <a href="#roadMap">ROADMAP</a>
-                <a href="#theTeam">TEAM</a>
-                <a href="#ourCommunity">COMMUNITY</a>
-                <a href="#ourLaunchpad">LAUNCHPAD</a>
-                <a href="#globalPenguinSociety">ASSOCIATION</a>
-                <a href="#faq">FAQ</a>
+                <a href="/#roadMap">ROADMAP</a>
+                <a href="/#theTeam">TEAM</a>
+                <a href="/#ourCommunity">COMMUNITY</a>
+                <a href="/#ourLaunchpad">LAUNCHPAD</a>
+                <a href="/#globalPenguinSociety">ASSOCIATION</a>
+                <a href="/#faq">FAQ</a>
                 <a href="/LITE PAPER.pdf">LITEPAPER</a>
             </div>
             <FollowUs />
